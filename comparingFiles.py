13f374f1e5783cffb97c82aeef78eea1cf94f60c
Naeus <naelonemaxwell@gmail.com>
2016-12-29 15:55:40 +0300

    testing MR

--- comparingFiles.py
+++ comparingFiles.py
@@ -1,15 +1,25 @@
 import filecmp
 
+mm = []
+
 def main():
-    filecmp.clear_cache()
+    n = 27
+    for i in range(n):
+        filecmp.clear_cache()
 
-    #f1 = r'D:\primes\lst32_mr\p[32].txt'
-    #f2 = r'D:\primes\lst32_td\p[32].txt'
+        f1 = r'D:\primes\lst32_mr\p' + str(i) + '.txt'
+        f2 = r'D:\primes\lst32_td\p' + str(i) + '.txt'
 
-    f1 = r'D:\primes\p.c'
-    f2 = r'D:\primes\p - Copy.c'
+        #f1 = r'D:\primes\p.c'
+        #f2 = r'D:\primes\p - Copy.c'
 
-    print(filecmp.cmp(f1, f2))
+        duplicate = filecmp.cmp(f1, f2)
+        print(duplicate)
+        if not duplicate:
+            mm.append(i)
+
+    print(mm)
+
 
 while True:
     main()
