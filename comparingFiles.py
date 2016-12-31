c2445a25b4c311f3f1f7d474be224a0cd7778308
Naeus <naelonemaxwell@gmail.com>
2016-12-31 09:40:44 +0300

    usage

--- comparingFiles.py
+++ comparingFiles.py
@@ -3,12 +3,13 @@ import filecmp
 mm = []
 
 def main():
-    n = 27
-    for i in range(n):
+    m = 0
+    n = 12
+    for i in range(n - m):
         filecmp.clear_cache()
 
-        f1 = r'D:\primes\lst32_mr\p' + str(i) + '.txt'
-        f2 = r'D:\primes\lst32_td\p' + str(i) + '.txt'
+        f1 = r'D:\primes\lst32_mr\p' + str(i+m) + '.txt'
+        f2 = r'D:\primes\lst32_td\p' + str(i+m) + '.txt'
 
         #f1 = r'D:\primes\p.c'
         #f2 = r'D:\primes\p - Copy.c'
@@ -16,7 +17,7 @@ def main():
         duplicate = filecmp.cmp(f1, f2)
         print(duplicate)
         if not duplicate:
-            mm.append(i)
+            mm.append(i+m)
 
     print(mm)
 
